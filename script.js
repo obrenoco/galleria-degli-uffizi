8fa62a215136446ff7f588574522479e3cba1a6f
Breno Romeiro <bsromeiro@gmail.com>
2022-10-04 23:37:34 -0300

    improve section 1 animation

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-let tlSection1 = gsap.timeline({ defaults: { duration: 0.8 } });
-tlSection1.from("h1", { y: 10, opacity: 0 }).from("img", { opacity: 0 }, 0);
+window.addEventListener("load", () => {
+  let tlSection1 = gsap.timeline({ defaults: { duration: 2, ease: "expo" } });
+  tlSection1
+    .from("h1", { y: 20, opacity: 0 })
+    .from("img", { opacity: 0, scale: 0.8 }, 0);
+});
