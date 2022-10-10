e0d40020702abf3822f7240e0b6991d3736c1a7c
Breno Romeiro <bsromeiro@gmail.com>
2022-10-10 17:15:31 -0300

    wip

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 gsap.registerPlugin(ScrollTrigger);
 
+function openMenu() {
+  console.log("ABRE MENU");
+}
+
+function closeMenu() {
+  console.log("FECHA MENU");
+}
+
 window.addEventListener("load", () => {
   let tlSection1 = gsap.timeline({ defaults: { duration: 2, ease: "expo" } });
   tlSection1
     .from("h1", { y: 20, opacity: 0 })
     .from(
-      "#section-1 img",
+      "#section-main img",
       {
         scale: 0.8,
       },
       0
     )
     .fromTo(
-      "#section-1",
+      "#section-main",
       { scale: 1 },
       {
         scale: 1.1,
         scrollTrigger: {
-          trigger: "#section-1",
+          trigger: "#section-main",
           start: "15% 10%",
           end: "bottom -100%",
           scrub: 1.5,
@@ -31,7 +39,7 @@ window.addEventListener("load", () => {
   //     scale: 1.8,
   //     y: 50,
   //     scrollTrigger: {
-  //       trigger: "#section-1",
+  //       trigger: "#section-main",
   //       start: "20% 20%",
   //       end: "bottom -100%",
   //       scrub: 1.5,
